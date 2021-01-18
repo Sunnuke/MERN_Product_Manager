d55796b0d2c6ceaf372dd84e5b85efbd6edf84a0
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-18 11:15:27 -0800

    Setup Main.js functional component and import it into App.js

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -1,1 +1,15 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
+
+export default () => {
+    const [message, setMessage] = useState("Please Wait!...");
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api")
+            .then(res => setMessage(res))
+    }, []);
+
+    return(
+        <h1>Success Message: {message}</h1>
+    )
+}
