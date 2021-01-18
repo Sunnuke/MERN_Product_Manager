2e04696176c50e091efbc1bbdc987e90b146bf1f
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-18 12:59:47 -0800

    Updated: product.routes.js

--- server/controller/product.controller.js
+++ server/controller/product.controller.js
@@ -1,7 +1,18 @@
-const { request, response } = require("express");
+const { Product } = require("../models/product.model");
 
 module.exports.index = (request, response) => {
     response.json({
         message: "Connected!"
     });
+}
+
+module.exports.ProductNew = (request, response) => {
+    const { title, price, description } = request.body;
+    Product.create({
+        title,
+        price,
+        description
+    })
+        .then(response => response.json(person))
+        .catch(err => response.json(err));
 }
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -3,4 +3,5 @@ const ProductController = require("../controller/product.controller");
 
 module.exports = (app) => {
     app.get("/api", ProductController.index);
+    app.post("/api/products/new", ProductController.ProductNew);
 }
