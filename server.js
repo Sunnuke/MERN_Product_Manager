960d166bef3a4556b1fcf40fd4e75c967f7af0dc
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-18 12:16:18 -0800

    Updated: Server.js imported config and set in use epxress.json and express.urlencoded(extended: true)

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const app = express();
 const port = 8000;
 
+require("./server/config/mongoose.config");
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 require("./server/routes/product.routes")(app);
 app.listen(port, () => console.log(`server.js: Listening to port ${port}`));
