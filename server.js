dc7abe2ca2d21c0f921594c4a7a57759850b2708
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-18 11:21:10 -0800

    Updated Server.js , Installed cors

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8000;
 
+app.use(cors());
+require("./server/routes/product.routes")(app);
 app.listen(port, () => console.log(`server.js: Listening to port ${port}`));
