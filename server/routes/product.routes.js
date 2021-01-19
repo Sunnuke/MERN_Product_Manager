d38ae03abfa51d0d38483706f09bcd8219c647bd
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-18 17:02:55 -0800

    Update: Added a findAll and findOne route to person.routes.js

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -3,5 +3,7 @@ const ProductController = require("../controller/product.controller");
 
 module.exports = (app) => {
     app.post("/api/products/new", ProductController.createProduct);
+    app.get("/api/products", ProductController.allProducts);
+    app.get("/api/products/:id", ProductController.oneProduct);
     app.get("/api", ProductController.index);
 }
