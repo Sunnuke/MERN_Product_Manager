dc071b3a495583f11d584e7c4db342ea5aa5abd2
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-19 14:04:48 -0800

    Update: Added an updateOne and deleteOne route to product.routes.js

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -2,8 +2,10 @@ const { get } = require("mongoose");
 const ProductController = require("../controller/product.controller");
 
 module.exports = (app) => {
-    app.post("/api/products/new", ProductController.createProduct);
-    app.get("/api/products", ProductController.allProducts);
-    app.get("/api/products/:id", ProductController.oneProduct);
     app.get("/api", ProductController.index);
+    app.get("/api/products", ProductController.allProducts);
+    app.post("/api/products/new", ProductController.createProduct);
+    app.get("/api/products/:id", ProductController.oneProduct);
+    app.put("/api/products/:id", ProductController.UpdateProduct);
+    app.delete("/api/products/:id", ProductController.deleteProduct);
 }
